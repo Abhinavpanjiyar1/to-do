ab696b339fb819423b29a7b0123b90dd5c3ef43c
Abhinavpanjiyar1 <Abhinavpanjiyar.is19@bmsce.ac.in>
2024-08-20 15:18:40 +0530

    Implement the edit button

--- python2/day17.py
+++ python2/day17.py
@@ -6,9 +6,12 @@ import PySimpleGUI as aa
 label = aa.Text("type in todo")
 input_box = aa.InputText(tooltip ="enter todo", key = "todo")
 add_button = aa.Button("add")
+list_box = aa.Listbox(values = functions.get_todos(), key = 'todos',
+                      enable_events =True, size = [45, 10])
+edit_button = aa.Button('edit')
 
 window = aa.Window("my todo app",
-                   layout = [[label], [input_box, add_button]],
+                   layout = [[label], [input_box, add_button], [list_box, edit_button]],
                    font =('Helvetica', 20))
 
 while True:
@@ -21,6 +24,20 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+
+        case "edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+
+        case "todo":
+            window['todos'].update(value= values[todos])
+
         case aa.WIN_CLOSED:
             break
 
