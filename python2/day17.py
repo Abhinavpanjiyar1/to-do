b70c9d5e495d6cff2ddadb4ae40259cb342f5622
Abhinavpanjiyar1 <Abhinavpanjiyar.is19@bmsce.ac.in>
2024-08-20 22:45:53 +0530

    final touch add clock

--- python2/day17.py
+++ python2/day17.py
@@ -2,7 +2,10 @@ import functions
 from functions import get_todos, write_todos
 
 import PySimpleGUI as aa
+import time
 
+aa.theme('black')
+clock = aa.Text('' ,key ='clock')
 label = aa.Text("type in todo")
 input_box = aa.InputText(tooltip ="enter todo", key = "todo")
 add_button = aa.Button("add")
@@ -13,19 +16,16 @@ complete_button = aa.Button('complete')
 exit_button = aa.Button("exit")
 
 window = aa.Window("my todo app",
-                   layout = [[label],
+                   layout = [[clock],
+                             [label],
                              [input_box, add_button],
                              [list_box, edit_button, complete_button],
                              [exit_button]],
                    font =('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values["todos"])
-    #print(event)
-    print(values)
+    event, values = window.read(timeout = 500)
+    window["clock"].update(value = time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "add":
             todos = functions.get_todos()
@@ -35,21 +35,27 @@ while True:
             window['todos'].update(values=todos)
 
         case "edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values = todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values = todos)
+            except IndexError:
+                aa.popup("pls select an item first")
         case "complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values = todos)
-            window['todo'].update(value = '')
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values = todos)
+                window['todo'].update(value = '')
+            except IndexError:
+                aa.popup("pls select an item first")
         case "exit":
             break
 
