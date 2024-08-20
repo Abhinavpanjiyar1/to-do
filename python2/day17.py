8fd9bf6e1d9e94b776e00c46d96631e163757d79
Abhinavpanjiyar1 <Abhinavpanjiyar.is19@bmsce.ac.in>
2024-08-20 22:25:21 +0530

    Implement the complete and exit button

--- python2/day17.py
+++ python2/day17.py
@@ -9,13 +9,21 @@ add_button = aa.Button("add")
 list_box = aa.Listbox(values = functions.get_todos(), key = 'todos',
                       enable_events =True, size = [45, 10])
 edit_button = aa.Button('edit')
+complete_button = aa.Button('complete')
+exit_button = aa.Button("exit")
 
 window = aa.Window("my todo app",
-                   layout = [[label], [input_box, add_button], [list_box, edit_button]],
+                   layout = [[label],
+                             [input_box, add_button],
+                             [list_box, edit_button, complete_button],
+                             [exit_button]],
                    font =('Helvetica', 20))
 
 while True:
     event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
     #print(event)
     print(values)
     match event:
@@ -35,9 +43,18 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values = todos)
+        case "complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+            window['todo'].update(value = '')
+        case "exit":
+            break
 
         case "todo":
-            window['todos'].update(value= values[todos])
+            window['todos'].update(value= values['todos'][0])
 
         case aa.WIN_CLOSED:
             break
